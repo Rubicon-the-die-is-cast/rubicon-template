023a040f0511ff9fa723d99356bcecc408465d33
Dauletkhan Narynbayev <dauletkhannarynbaev@gmail.com>
2026-03-31 00:08:20 +0500

    fix: use _copier_conf.dst_path.name, remove broken local extension

--- copier_extensions.py
+++
@@ -1,9 +0,0 @@
-import os
-from pathlib import Path
-from jinja2.ext import Extension
-
-
-class FolderNameExtension(Extension):
-    def __init__(self, environment):
-        super().__init__(environment)
-        environment.globals["_folder_name"] = Path(os.getcwd()).name
